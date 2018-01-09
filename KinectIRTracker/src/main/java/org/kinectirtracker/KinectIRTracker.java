b40e8c4bd93849ee4ef6b741869ea62d96771593
Michael Cao <michael.cao@alumni.ubc.ca>
2018-01-08 18:36:37 -0800

    adding jna library, add code to callback once a frame is read from the kinect

--- KinectIRTracker/src/main/java/org/kinectirtracker/KinectIRTracker.java
+++ KinectIRTracker/src/main/java/org/kinectirtracker/KinectIRTracker.java
@@ -1,13 +1,45 @@
 package main.java.org.kinectirtracker;
 
-public class KinectIRTracker {
+import java.nio.ByteBuffer;
 
+import org.openkinect.freenect.Context;
+import org.openkinect.freenect.Device;
+import org.openkinect.freenect.FrameMode;
+import org.openkinect.freenect.Freenect;
+import org.openkinect.freenect.VideoFormat;
+import org.openkinect.freenect.VideoHandler;
+
+public class KinectIRTracker {
+	private static final int KINECT_DEVICE = 0;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Context context = Freenect.createContext();
+		
+		try {
+			Device kinect = context.openDevice(KINECT_DEVICE);
+			
+			kinect.refreshTiltState();
+			kinect.setVideoFormat(VideoFormat.IR_8BIT);
+			
+			kinect.startVideo(new VideoHandler() {
+				public void onFrameReceived(FrameMode mode, ByteBuffer frame, int timestamp) {
+					// TODO Auto-generated method stub
+					System.out.println("TEST");
+				}
+			});
 
+			// infinite loop while reading frames
+			while(true) {
+				
+			}			
+		}
+		catch (Exception e) {
+			System.out.println("ERROR: " + e.getMessage());
+			return;
+		}
 	}
 
 }
